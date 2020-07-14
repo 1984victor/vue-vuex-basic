2a7958d266f1243a95e4193c1e751972ec16a5e3
1984victor <1984.victor@gmail.com>
2020-07-14 10:44:48 +0800

    3.7 Access to multiple module computed attributes no namespace

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h2>My name is Index.vue</h2>
     <h3>Counter B $store.state.b.counter is : {{ $store.state.b.counter }} </h3>
-    <h3>asyncModify B Counter : <button v-on:click="$store.dispatch('b/increaseAction')">asyncModify B Counter</button></h3>
+    <h3>Get B $store.getters.doubleCounterB : {{$store.getters.doubleCounterB}} </h3>
   </div>
 </template>
 <script>
